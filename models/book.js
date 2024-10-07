c9781bd29230f2c352d2ae45ec41187d534c1611
Jacqueline Offei <offeijacqueline@gmail.com>
2024-10-07 17:44:07 +0000

    adding functions and routes true

--- models/book.js
+++ models/book.js
@@ -1,3 +1,14 @@
-import { Schema, model } from "mongoose";
+import { Schema, model, Types } from "mongoose";
 
-const bookModel = new 
+
+// create a new table, stored in bookModel
+const bookModel = new Schema({
+  title: { type: String, required: true },
+  summary: { type: String, required: true },
+  author: { type: Types.ObjectId, ref: "Author" },
+  cover: { type: String, required: true },
+  content: { type: String, required: true },
+});
+
+// export the stored values as a class
+export const BookModel = model("Books", bookModel);
