e6e26eeb1628ec9eb22a6034e78399252b8d320a
ownyanteh <ownyanteh@gmail.com>
2024-10-07 17:45:10 +0000

    added author funtions true

---
+++ models/author.js
@@ -0,0 +1,9 @@
+import { Schema , model} from "mongoose";
+
+const authorModel = new Schema ({
+    firstName: {type: String, requiured: true},
+    lastName: {type: String, requiured: true},
+    bio: {type: String, requiured: true},
+})
+
+export const AuthorModel = model ("Author", authorModel)
