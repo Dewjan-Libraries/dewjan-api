fcdd1acd3768c209d032526d30e1a5e830107a3a
Jacqueline Offei <offeijacqueline@gmail.com>
2024-10-07 16:33:42 +0000

    created functions

---
+++ controllers/book.js
@@ -0,0 +1,15 @@
+
+export  const getBook = (req, res, next) => {
+    res.json('book was added');
+}
+export  const getBooks = (req, res, next) => {
+    res.json('books were added');
+}
+export  const updateBook = (req, res, next) => {
+    res.json('book was updated');
+}
+export  const deleteBook = (req, res, next) => {
+    res.json('book was deleted');
+}
+
+
---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+
+
+//  create  an express app
+
+const app = express();
+
+
+// create server
+app.listen(3000, () => {
+    console.log("app is listening");
+});
