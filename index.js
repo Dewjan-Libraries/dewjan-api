ad16c636edd96b4a3646519408b4108bfbd5a1d7
Jacqueline Offei <offeijacqueline@gmail.com>
2024-10-07 19:03:56 +0000

    adding cors

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import express from "express";
-
-
+import bookRouter from "./routes/book.js";
+import cors from "cors";
 
 //  create  an express app
 
 const app = express();
 
+// creating middlewares
+app.use(express.json());
+app.use(cors);
+app.use(bookRouter);
 
 // create server
 app.listen(3000, () => {
-    console.log("app is listening");
+  console.log("app is listening");
 });
