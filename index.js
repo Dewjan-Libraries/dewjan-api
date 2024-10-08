c8883951ccb25bd12e765a2b58d19be7aadcb02e
Jacqueline Offei <offeijacqueline@gmail.com>
2024-10-08 14:32:48 +0000

    adding author routes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bookRouter from "./routes/book.js";
 import cors from "cors";
 import mongoose from "mongoose";
 import reviewRouter from "./routes/reviews.js";
+import authorRouter from "./routes/author.js";
 
 // connect database
 await mongoose.connect(process.env.MONGO_URI);
@@ -16,7 +17,7 @@ app.use(express.json());
 app.use(cors);
 app.use(bookRouter);
 app.use(reviewRouter);
-
+app.use(authorRouter)
 // create server
 app.listen(3000, () => {
   console.log("app is listening");
--- routes/author.js
+++ routes/author.js
@@ -1,2 +1,22 @@
+import { Router } from "express";
+import {
+  addAuthor,
+  deleteAuthor,
+  getAuthor,
+  getAuthors,
+  updateAuthor,
+} from "../controllers/author.js";
 
+const authorRouter = Router();
 
+authorRouter.post("/author", addAuthor);
+
+authorRouter.get("/author/:id", getAuthor);
+
+authorRouter.get("/author", getAuthors);
+
+authorRouter.patch("/author", updateAuthor);
+
+authorRouter.delete("/author", deleteAuthor);
+
+export default authorRouter;
