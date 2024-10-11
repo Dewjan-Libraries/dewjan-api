1667b234757143ed12fb03e39fc819cfe34acb76
Jacqueline Offei <offeijacqueline@gmail.com>
2024-10-11 12:54:49 +0000

    adding removing required

--- models/book.js
+++ models/book.js
@@ -2,11 +2,11 @@ import { Schema, model, Types } from "mongoose";
 
 // create a new table, stored in bookModel
 const bookModel = new Schema({
-  title: { type: String, required: true },
-  summary: { type: String, required: true },
+  title: { type: String },
+  summary: { type: String },
   author: { type: Types.ObjectId, ref: "Author" },
-  genre: { type: String, required: true },
-  description: { type: String, required: true },
+  genre: { type: String },
+  description: { type: String },
   publishDate: { type: Number },
   image: { type: String },
   pdf: { type: String },
